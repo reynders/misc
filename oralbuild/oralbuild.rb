46a1d787ab5155daa9016fb8326823a4653be511
Joonas Reynders <joonas.reynders@iki.fi>
2011-04-18 12:42:55 +0300

    Fixed url

--- oralbuild/oralbuild.rb
+++ oralbuild/oralbuild.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 old_status = ""
 
 while true do
-  current_status = open("http://inrad.research.nokia.com").read.scan(/<div class="subsystem ([^>]*)">[^<]*<div[^<]*<h3[^<]*<a[^>]*>Qt Components</m)[0][0]
+  current_status = open("http://inrad.research.foo.com").read.scan(/<div class="subsystem ([^>]*)">[^<]*<div[^<]*<h3[^<]*<a[^>]*>Qt Components</m)[0][0]
   if current_status != old_status
     command = "echo build " + current_status.to_s + " | espeak"
     puts "Command is #{command}"
